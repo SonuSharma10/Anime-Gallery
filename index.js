e819f82279d05ab6c080cf662f9da8264d2dd1c9
SonuSharma10 <sonu.g.sharma10@gmail.com>
2024-07-09 20:52:56 +0530

    new

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const path = 8080;
+const { v4: uuidv4 } = require('uuid');
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+app.use(express.urlencoded({ extended: true }));
 
+//declaration of sample data
 posts = [
   {
+    id: uuidv4(),
     username: 'Sonu Sharma',
     thaught: 'Life is Great ❤️',
     image: [
@@ -15,6 +19,7 @@ posts = [
     ],
   },
   {
+    id: uuidv4(),
     username: 'Sahil Sharma',
     thaught: 'Working hard is one way to success',
     image: [
@@ -24,6 +29,7 @@ posts = [
     ],
   },
   {
+    id: uuidv4(),
     username: 'Mighty Raju',
     thaught: "Don't provoke indians",
     image: [
@@ -34,31 +40,36 @@ posts = [
   },
 ];
 
-app.use(express.urlencoded({ extended: true }));
-
+//fetching index.js on /posts
 app.get('/posts', (req, res) => {
   app.use(express.static(__dirname + '/public'));
   res.render('index.ejs', { posts });
 });
 
+//new post creation request on /posts/new
 app.get('/posts/new', (req, res) => {
   res.render('new_thaught.ejs');
 });
 
+//update new post in /posts and redirect there
 app.post('/posts', (req, res) => {
   console.log(req.body);
   let img = [];
   let { username, thaught, image } = req.body;
+  id = uuidv4();
+  console.log(id);
   //NOTE { username,thaught} === {username:username, thaught:thaught}
   if (image.constructor != Array) {
     img.push(image);
   } else {
     img = image;
   }
-  posts.push({ username, thaught, image: img });
+  posts.push({ id, username, thaught, image: img });
+  console.log(posts);
   res.redirect('/posts');
 });
 
+//see post based on id in detail option=>Click here to see All xyz posts
 app.listen(path, () => {
   console.log('listning on port', path);
 });
